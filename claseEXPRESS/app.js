6b7f04ce857e548a92273f39ca94164bc7f45441
Randu Retana <randu2002@hotmail.com>
2023-02-28 11:01:24 -0600

    Agrega explicación de clase express

---
+++ claseEXPRESS/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    //('¡Hola mundo!'); //Manda la respuesta
+    next();
+});
+
+
+
+const hockeyRutas = require('../routes/hockey.routes');
+
+app.use('/hockey', hockeyRutas)
+
+app.use((request, response, next) =>{
+    console.log("Tercer middleware!");
+
+    response.status(404);
+    response.send("Lo sentimos, esta ruta no existe");
+    next();
+})
+app.listen(3000);
---
+++ lab10/app.js
@@ -0,0 +1,30 @@
+let http = require('http');
+let fs = require('fs');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    if(request.url === "/"){
+        fs.readFile('./paginalab10.html', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+    else if(request.url==="/opciones"){
+        response.write("SEXOOOOO");
+        response.end();
+        
+    }
+    else if(request.url === "/inicio"){
+        response.write("Bienvenido a tu psicologo ONLINE!")
+    }
+    
+};
+
+http.createServer(handleRequest).listen(7000);
